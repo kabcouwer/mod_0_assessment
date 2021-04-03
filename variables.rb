5b9b4ea66c5ab6103e27520595ae385e6b5a4571
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-04-02 20:25:11 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "jaws"
+is_entertaining = true
+movie = "mulan"
+num_4 = 2
+num_6 = 10
+product = num_4 * num_6
+favorite_foods = ["tacos", "pizza", "burger"]
+favorite_foods.shift
+puts movie.size
