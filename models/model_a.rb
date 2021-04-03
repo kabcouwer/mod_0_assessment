4d61cfbaabafa0b8c8b648faf8f74aebdb56e67c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-04-02 20:46:32 -0600

    Add variable dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,9 @@
+dinosaur = {"name" => "brachiosaurus",
+  "genus" => "sauropod",
+  "legth_feet" => 65,
+  "weight_metric_tons" => 45,
+  "is_vegetarian" = true,
+  "is_small" = false,
+  "nicknames" = ["deep_chest", "arm_lizard"],
+  "centuries" = [19, 20]
+}
