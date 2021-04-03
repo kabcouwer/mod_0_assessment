594cb21117a0e7b006dfdcdeabfb4d50c20c6495
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-04-02 20:49:12 -0600

    Update with hash rocket

--- models/model_a.rb
+++ models/model_a.rb
@@ -2,8 +2,8 @@ dinosaur = {"name" => "brachiosaurus",
   "genus" => "sauropod",
   "legth_feet" => 65,
   "weight_metric_tons" => 45,
-  "is_vegetarian" = true,
-  "is_small" = false,
-  "nicknames" = ["deep_chest", "arm_lizard"],
-  "centuries" = [19, 20]
+  "is_vegetarian" => true,
+  "is_small" => false,
+  "nicknames" => ["deep_chest", "arm_lizard"],
+  "centuries" => [19, 20]
 }
