07a845b94c3dce04b094a908ae113de19ad0e875
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-04-02 20:55:01 -0600

    Add variable developer

---
+++ models/model_b.rb
@@ -0,0 +1,9 @@
+developer = {"name" => "Kim",
+  "gender" => "female",
+  "age" => 32,
+  "birth_year" => 1989,
+  "is_graduated" => false,
+  "is_queer" => true,
+  "nicknames" => ["kimbo", "kiki"],
+  "lived" => ["texas", "indiana", "west_virginia", "colorado"]
+}
